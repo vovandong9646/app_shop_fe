eb4c971880ef4e10e7471e06b391c18c52b0f113
dongvo <vovandong14i1@gmail.com>
2024-07-30 21:02:32 +0700

    add init

---
+++ src/components/auth/AclGuard.tsx
@@ -0,0 +1,20 @@
+// ** React Imports
+import { ReactNode } from 'react'
+
+// ** Types
+import type { ACLObj } from 'src/configs/acl'
+
+interface AclGuardProps {
+  children: ReactNode
+  authGuard?: boolean
+  guestGuard?: boolean
+  aclAbilities: ACLObj
+}
+
+const AclGuard = (props: AclGuardProps) => {
+  // ** Props
+  const { aclAbilities, children, guestGuard = false, authGuard = true } = props
+  return <>{children}</>
+}
+
+export default AclGuard
