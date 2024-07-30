d2a81e7e20c2d0aa495b9e7175b828457d226b4a
dongvo <vovandong14i1@gmail.com>
2024-07-30 21:31:40 +0700

    update format

--- src/components/auth/AclGuard.tsx
+++ src/components/auth/AclGuard.tsx
@@ -14,6 +14,7 @@ interface AclGuardProps {
 const AclGuard = (props: AclGuardProps) => {
   // ** Props
   const { aclAbilities, children, guestGuard = false, authGuard = true } = props
+
   return <>{children}</>
 }
 
